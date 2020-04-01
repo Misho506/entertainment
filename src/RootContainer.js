060d4193f5498d88ae000a9df60a246d900df746
Mauricio Lopez <mlopez@pernixlabs.com>
2020-03-31 23:10:44 -0600

    Add bootstrap

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker'
+
+import './assets/styles/main.scss'
 class RootContainer extends React.Component {
 
   render() {
